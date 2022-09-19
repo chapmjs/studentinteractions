3a3aa1d6960ad0ef7a4d89607f11d3aa8e347083
John Chapman <chapmjs@gmail.com>
2022-09-19 14:15:16 -0600

    first commit

---
+++ studentinteractionsapp.R
@@ -0,0 +1,37 @@
+# student interactions app
+# main page
+
+# libraries
+library(shiny)
+
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+  # Application title
+  titlePanel("Student Interactions App"),
+
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    sidebarPanel(
+
+
+    ),
+
+    # Show a plot of the generated distribution
+    mainPanel(
+
+
+    )
+  )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+
+
+}
+
+# Run the application
+shinyApp(ui = ui, server = server)
