31356138b0d9a3d0e74988f52af174ae214d8f8d
John Chapman <chapmjs@gmail.com>
2023-10-26 16:44:31 -0600

    tab design

--- studentinteractionsapp.R
+++ studentinteractionsapp.R
@@ -4,26 +4,32 @@
 # libraries
 library(shiny)
 
+# Pull data
+
+student.name = c("John","Kathryn","Joshua","Anna")
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
-  # Application title
-  titlePanel("Student Interactions App"),
-
-  # Sidebar with a slider input for number of bins
-  sidebarLayout(
-    sidebarPanel(
-
-
-    ),
-
-    # Show a plot of the generated distribution
-    mainPanel(
-
+  tabsetPanel(
+    tabPanel("Record interaction",
+             selectInput("student", "Which student?", student.name),
+             textAreaInput("interactiontext", "Enter notes:", rows = 4, cols = 40),
+             actionButton("createnew", "Create New Student")
+             ),
+    tabPanel("New Student",
+             textInput("name","Student Name"),
+             dateInput("creationdate","Date"),
+             textAreaInput("student.notes", "Enter student notes:", rows = 4, cols = 40),
+             actionButton("createnew", "Create New Student")
 
     )
   )
+
+
+
+
 )
 
 # Define server logic required to draw a histogram
